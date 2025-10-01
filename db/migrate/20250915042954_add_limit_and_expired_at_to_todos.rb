abe1e793db2432fed18e478e06020c555f07cbcf
vantrong <trongdn2405@gmail.com>
2025-10-01 23:58:00 +0700

    add rules and fix rubocop

--- db/migrate/20250915042954_add_limit_and_expired_at_to_todos.rb
+++ db/migrate/20250915042954_add_limit_and_expired_at_to_todos.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class AddLimitAndExpiredAtToTodos < ActiveRecord::Migration[8.0]
   def change
     add_column :todos, :limit, :integer
--- db/migrate/20250916033125_create_todo_jobs.rb
+++ db/migrate/20250916033125_create_todo_jobs.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class CreateTodoJobs < ActiveRecord::Migration[8.0]
   def change
     create_table :todo_jobs do |t|
@@ -7,8 +9,8 @@ class CreateTodoJobs < ActiveRecord::Migration[8.0]
       t.timestamps
       t.datetime :deleted_at
 
-      t.index [ :todo_id ]
-      t.index [ :status ]
+      t.index [:todo_id]
+      t.index [:status]
     end
 
     add_foreign_key :todo_jobs, :todos
